7c25119f118b377e49d75dfeba7aeae031349c01
Nathan Herald <me@nathanherald.com>
2017-10-24 10:42:42 -0400

    Remove some more comments

--- config/test.exs
+++ config/test.exs
@@ -1,15 +1,11 @@
 use Mix.Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :authority, AuthorityWeb.Endpoint,
   http: [port: 4001],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
 config :authority, Authority.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("USER"),
